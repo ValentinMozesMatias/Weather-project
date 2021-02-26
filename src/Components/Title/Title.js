3c77bc7b87f3bdc901a6feb4617c8fb71094633e
Valentin Mozes <valentin.mozes@gmail.com>
2021-02-26 22:43:39 +0200

    I added a comment in title.js

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../Container.css'
 
+// This component is reusable for any title in the project
 const Title = ({ text }) => <h3>{text}</h3>
 
 export default Title
